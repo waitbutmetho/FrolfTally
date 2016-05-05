1be8616a768a7ee03c48b0671dd64c9b98b95a41
Joshua Gustafson <silversevenfox@yahoo.com>
2016-05-05 09:45:58 -0700

    add aditiona youtube api stuff

--- app/src/main/java/com/example/josh/myapplication/VideoActivity.java
+++ app/src/main/java/com/example/josh/myapplication/VideoActivity.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class VideoActivity extends AppCompatActivity implements View.OnClickListener {
+    private String GOOGLE_API_KEY = "AIzaSyCgpp39acbHjGYieMlUhkBaejHSQYpFCSs";
+    private String YOUTUBE_VIDEO_ID = "TLowvHyhSWU";
+
     public static final String TAG = HomeActivity.class.getSimpleName();
-    @Bind(R.id.whatIsDiscGolfBtn)
-    Button mWhatIsDiscGolfBtn;
+    @Bind(R.id.whatIsDiscGolfBtn) Button mWhatIsDiscGolfBtn;
+    @Bind(R.id.howToThrowBackhandBtn) Button mHowToThrowBackhandBtn;
+    @Bind(R.id.howToPuttBtn) Button mHowToPuttBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,29 @@ public class VideoActivity extends AppCompatActivity implements View.OnClickList
         ButterKnife.bind(this);
 
         mWhatIsDiscGolfBtn.setOnClickListener(this);
+        mHowToThrowBackhandBtn.setOnClickListener(this);
+        mHowToPuttBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v == mWhatIsDiscGolfBtn) {
-            Intent intent = new Intent(VideoActivity.this, WhatIsDiscGolfVideoActivity.class);
+        Intent intent = null;
+
+        switch(v.getId()) {
+            case R.id.whatIsDiscGolfBtn:
+                intent = new Intent(VideoActivity.this, WhatIsDiscGolfVideoActivity.class);
+                break;
+
+            case R.id.howToPuttBtn:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY, YOUTUBE_VIDEO_ID);
+                break;
+
+            case R.id.howToThrowBackhandBtn:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY, YOUTUBE_VIDEO_ID);
+                break;
+        }
+
+        if(intent != null) {
             startActivity(intent);
         }
     }
