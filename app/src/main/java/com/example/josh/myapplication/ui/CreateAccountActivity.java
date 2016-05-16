712c82670a3410fb74eb90003766f84d82331042
Joshua Gustafson <silversevenfox@yahoo.com>
2016-05-16 11:25:16 -0700

    add logout

--- app/src/main/java/com/example/josh/myapplication/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/josh/myapplication/ui/CreateAccountActivity.java
@@ -1,5 +1,8 @@
 package com.example.josh.myapplication.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,10 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.josh.myapplication.Constants;
 import com.example.josh.myapplication.R;
 import com.example.josh.myapplication.models.User;
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
@@ -21,19 +26,15 @@ import butterknife.ButterKnife;
 
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = CreateAccountActivity.class.getSimpleName();
-    @Bind(R.id.createUserButton)
-    Button mCreateUserButton;
-    @Bind(R.id.nameEditText)
-    EditText mNameEditText;
-    @Bind(R.id.emailEditText)
-    EditText mEmailEditText;
-    @Bind(R.id.passwordEditText)
-    EditText mPasswordEditText;
-    @Bind(R.id.confirmPasswordEditText)
-    EditText mConfirmPasswordEditText;
-    @Bind(R.id.loginTextView)
-    TextView mLoginTextView;
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
     private Firebase mFirebaseRef;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +43,20 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         ButterKnife.bind(this);
         mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
         mCreateUserButton.setOnClickListener(this);
+        mLoginTextView.setOnClickListener(this);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(CreateAccountActivity.this);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
     }
 
     @Override
     public void onClick(View view) {
         if (view == mCreateUserButton) {
             createNewUser();
+        }
+        if (view == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }
     }
 
@@ -68,6 +77,42 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             public void onSuccess(Map<String, Object> result) {
                 String uid = result.get("uid").toString();
                 createUserInFirebaseHelper(name, email, uid);
+                mFirebaseRef.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        if (authData != null) {
+                            String userUid = authData.getUid();
+
+                            String userInfo = authData.toString();
+                            Log.d(TAG, "Currently logged in: " + userInfo);
+
+                            mSharedPreferencesEditor.putString(Constants.KEY_UID, userUid).apply();
+                            Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        switch (firebaseError.getCode()) {
+                            case FirebaseError.INVALID_EMAIL:
+                            case FirebaseError.USER_DOES_NOT_EXIST:
+                                mEmailEditText.setError("Please check that you entered your email correctly");
+                                break;
+                            case FirebaseError.INVALID_PASSWORD:
+                                mEmailEditText.setError(firebaseError.getMessage());
+                                break;
+                            case FirebaseError.NETWORK_ERROR:
+                                showErrorToast("There was a problem with the network connection");
+                                break;
+                            default:
+                                showErrorToast(firebaseError.toString());
+                        }
+                    }
+                });
             }
 
             @Override
@@ -76,5 +121,8 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                         firebaseError);
             }
         });
+    }
+    private void showErrorToast(String message) {
+        Toast.makeText(CreateAccountActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/josh/myapplication/ui/HomeActivity.java
+++ app/src/main/java/com/example/josh/myapplication/ui/HomeActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.josh.myapplication.Constants;
 import com.example.josh.myapplication.R;
+import com.firebase.client.Firebase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,6 +17,10 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     public static final String TAG = HomeActivity.class.getSimpleName();
     @Bind(R.id.scoreTrackerBtn) Button mScoreTrackerBtn;
     @Bind(R.id.discVideosBtn) Button mDiscVideosBtn;
+    @Bind(R.id.logoutBtn) Button mLogoutBtn;
+
+    private Firebase mFirebaseRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
 
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+
         mScoreTrackerBtn.setOnClickListener(this);
         mDiscVideosBtn.setOnClickListener(this);
+        mLogoutBtn.setOnClickListener(this);
     }
 
     @Override
@@ -36,6 +45,21 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(HomeActivity.this, VideoActivity.class);
             startActivity(intent);
         }
+        if (v == mLogoutBtn) {
+            logout();
+            takeUserToLoginScreenOnUnAuth();
+        }
 
+    }
+
+    protected void logout() {
+        mFirebaseRef.unauth();
+    }
+
+    private void takeUserToLoginScreenOnUnAuth() {
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
